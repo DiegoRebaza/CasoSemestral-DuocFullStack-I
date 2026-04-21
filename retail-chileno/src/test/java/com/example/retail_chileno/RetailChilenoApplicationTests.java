39b45002c210d5c4a2a6131bef5c810f91f488fe
Kerkezo <di.rebaza@duocuc.cl>
2026-04-21 16:03:48 -0400

    Carga inicial del proyecto

---
+++ retail-chileno/src/test/java/com/example/retail_chileno/RetailChilenoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retail_chileno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailChilenoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
