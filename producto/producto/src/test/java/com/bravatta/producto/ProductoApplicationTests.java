22281b3e8ad63447675099d0a0b054741c4d8573
Nublao <di.rebaza@duocuc.cl>
2026-05-12 16:12:08 -0400

    Subida de microservicios + gateway + auth

---
+++ producto/producto/src/test/java/com/bravatta/producto/ProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bravatta.producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
