80eba61cadb7809c4024307ce0d47726454116bb
Matttvz <mattvtorres29@gmail.com>
2026-06-30 15:24:31 -0400

    modificacion inventario

--- inventario/inventario/src/main/java/com/bravatta/inventario/config/SwaggerConfig.java
+++ inventario/inventario/src/main/java/com/bravatta/inventario/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package com.bravatta.inventario.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,10 +13,22 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
-            .info(new Info()
-                .title("API 2026 - Microservicio de Inventario")
-                .version("1.0")
-                .description("Documentacion automatizada de endpoints de inventario"));
+                .info(new Info()
+                        .title("API 2026 - Microservicio de Inventario")
+                        .version("1.0")
+                        .description("Documentacion automatizada de endpoints de inventario"))
+                // Esto agrega el candado a CADA endpoint en la UI
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                // Esto define CÓMO se ve el esquema (botón Authorize arriba a la derecha)
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
