a4929a5cbb8b3996674f87fec22cba64d859f712
Nublao <di.rebaza@duocuc.cl>
2026-07-13 09:42:23 -0400

    Update and rename detener-docker.bat to iniciar-docker.bat

--- iniciar-docker.bat
+++ iniciar-docker.bat
@@ -1,5 +1,5 @@
 @echo off
 echo ===================================
-echo  Deteniendo y limpiando contenedores Bravatta
+echo  Levantando Bravatta con Docker Compose
 echo ===================================
-docker compose down
+docker compose up --build 
